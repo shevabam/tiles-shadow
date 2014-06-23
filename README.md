7d475361927b42f7a69ff8c1980ceaf545b7eb39
ShevAbam <shevabam@gmail.com>
2014-06-23 14:31:25 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+![](screenshot.png)
+
+A simple tiles grid with nice effect.
+
+Watch in action : http://codepen.io/ShevAbam/pen/IvKLq
+
+*You must have SASS 3.3.7+ and Compass 1.0+ to compile (use of maps functions).*
---
+++ config.rb
@@ -0,0 +1,13 @@
+environment = :development
+
+output_style = (environment == :production) ? :compressed : :expanded
+
+http_path = "/"
+css_dir = "."
+sass_dir = "."
+images_dir = "images"
+fonts_dir = "fonts"
+javascripts_dir = "js"
+relative_assets = true
+line_comments = false
+asset_cache_buster :none
